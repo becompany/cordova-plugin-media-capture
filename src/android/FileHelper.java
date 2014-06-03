63dd3d118cd45c5c7c47010b78e8423934a39389
Roman Fuerst <roman.fuerst@becompany.ch>
2014-06-03 15:01:09 +0200

    Fix package name.

--- src/android/FileHelper.java
+++ src/android/FileHelper.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
  */
-package org.apache.cordova.mediacapture;
+package ch.becompany.cordova.mediacapture;
 
 import android.net.Uri;
 import android.webkit.MimeTypeMap;
